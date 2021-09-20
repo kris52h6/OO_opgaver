1a84275e80ea0daf4994933409fb83bc2b7ef1ea
kris52h6 <kris52h6@stud.kea.dk>
2021-09-20 14:23:48 +0200

    Dice

---
+++ src/com/company/Dice.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Random;
+
+public class Dice {
+    private int faceValue;
+
+    public Dice() {
+        this.faceValue = roll();
+    }
+
+    public int roll() {
+        Random rand = new Random();
+        int number = rand.nextInt(6);
+        number += 1;
+        return number;
+    }
+
+    public int getFaceValue() {
+        return faceValue;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,14 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-    Person person = new Person("Kristian", 24);
+        // Opgave 1 - Person
+        //
+
+        Person person = new Person("Kristian", 24);
         // define name & age
         String name = person.getName();
         int age = person.getAge();
@@ -17,5 +22,14 @@ public class Main {
 
         // print object
         System.out.println(person);
+
+        // Opgave 2 - Dice
+        //
+
+        Dice dice = new Dice();
+        int diceValue = dice.getFaceValue();
+
+        System.out.print("The random dice value is: ");
+        System.out.println(diceValue);
     }
 }
