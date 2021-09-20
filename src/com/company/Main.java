cfdaa8dd70c33c6cc44d10583671da4ec1eb7390
kris52h6 <kris52h6@stud.kea.dk>
2021-09-20 14:05:39 +0200

    Personopgave

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    Person person = new Person("Kristian", 24);
+        // define name & age
+        String name = person.getName();
+        int age = person.getAge();
+
+        // print name and age
+        System.out.println(name);
+        System.out.println(age);
+
+        // increase age by 1
+        person.hasBirthday();
+
+        // print object
+        System.out.println(person);
     }
 }
